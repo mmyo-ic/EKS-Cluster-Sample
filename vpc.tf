ff38748ee9351ba9939a68e9be9b3bd209347f31
mmyo-ic <103523721+mmyo-ic@users.noreply.github.com>
2022-05-01 08:57:46 -0400

    Update vpc.tf
    1- vpc name
    2- cidr
    3- subnets

--- vpc.tf
+++ vpc.tf
@@ -22,11 +22,11 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.2.0"
 
-  name                 = "education-vpc"
-  cidr                 = "10.0.0.0/16"
+  name                 = "demo-vpc"
+  cidr                 = "10.10.0.0/16"
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  private_subnets      = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
+  public_subnets       = ["10.10.4.0/24", "10.10.5.0/24", "10.10.6.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
