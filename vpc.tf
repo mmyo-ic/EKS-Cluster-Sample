e12a1d23506be81640c7164cc95e238a277dc629
mmyo-ic <103523721+mmyo-ic@users.noreply.github.com>
2022-05-02 09:42:38 -0400

    Update vpc.tf
    change back to network: cidr and subnets

--- vpc.tf
+++ vpc.tf
@@ -23,10 +23,10 @@ module "vpc" {
   version = "3.2.0"
 
   name                 = "ic-demo-vpc"
-  cidr                 = "10.10.0.0/16"
+  cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
-  public_subnets       = ["10.10.4.0/24", "10.10.5.0/24", "10.10.6.0/24"]
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
